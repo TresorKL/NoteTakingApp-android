865a688fc2cf05d1b29216b85d11f129fc7cb608
TresorKL <trezmathieu@gmail.com>
2022-08-27 12:06:11 +0200

    implement add button

---
+++ app/src/main/java/com/example/notetakingapp/note/Note.java
@@ -0,0 +1,65 @@
+package com.example.notetakingapp.note;
+
+import java.time.LocalDate;
+
+public class Note {
+
+
+    private Long id;
+    private LocalDate dateOfEdition;
+    private String title;
+    private String noteBody;
+    private String noteBackgroundColor;
+
+    public Note(Long id, LocalDate dateOfEdition, String title, String noteBody, String noteBackgroundColor) {
+        this.id = id;
+        this.dateOfEdition = dateOfEdition;
+        this.title = title;
+        this.noteBody = noteBody;
+        this.noteBackgroundColor = noteBackgroundColor;
+    }
+
+
+    public Note() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getDateOfEdition() {
+        return dateOfEdition;
+    }
+
+    public void setDateOfEdition(LocalDate dateOfEdition) {
+        this.dateOfEdition = dateOfEdition;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getNoteBody() {
+        return noteBody;
+    }
+
+    public void setNoteBody(String noteBody) {
+        this.noteBody = noteBody;
+    }
+
+    public String getNoteBackgroundColor() {
+        return noteBackgroundColor;
+    }
+
+    public void setNoteBackgroundColor(String noteBackgroundColor) {
+        this.noteBackgroundColor = noteBackgroundColor;
+    }
+}
